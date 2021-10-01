fc56a626719e66bcf70f1af272cddc154c5b66e1
Viegas <milton.viegas.br@gmail.com>
2021-10-01 15:36:00 -0300

    first commit

---
+++ Exercicios12/Exerc12/src/Evento/EventoFabrica.java
@@ -0,0 +1,25 @@
+package Evento;
+
+import java.util.Scanner;
+
+public class EventoFabrica {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int seg,hrs,min,segs;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("Duração do evento na fábrica em segundos: ");
+		seg = leia.nextInt();
+		
+		hrs = (seg*1)/3600;
+		min = (seg*1)/60;
+		segs = seg*1;
+		
+		System.out.println("O evento durou: "+hrs+" Horas "+min+" Minutos e "+segs+" Segundos");
+		leia.close();
+	}
+
+}
---
+++ Exercicios12/Exerc12/src/Inteiros/InteirosPositivos.java
@@ -0,0 +1,29 @@
+package Inteiros;
+
+import java.util.Scanner;
+
+public class InteirosPositivos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n1,n2,n3;
+		double D,R,S;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("Entre com o 1ª numero: ");
+		n1 = leia.nextInt();
+		System.out.println("Entre com o 2ª numero: ");
+		n2 = leia.nextInt();
+		System.out.println("Entre com o 3ª numero: ");
+		n3 = (int) leia.nextFloat();
+		
+		R = Math.pow(n1+3, 2);
+		S = Math.pow(n2+n3, 2);
+		D = (R+S)/2;
+		System.out.printf("O valor da expressão é: %2.2f",D);
+		leia.close(); 
+	}
+
+}
